7aea102db448cde687964003094fbbe891516fb2
omarfaruk <creaktube@gmail.com>
2026-04-22 11:45:55 +0600

    ultra pro max portfolio

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-body {
-  background-color: #1a1b26;
-  color: white;
-  text-align: center;
-  margin-top: 100px;
-  font-family: Arial;
-}
-
-button {
-  padding: 10px 20px;
-  background: #7aa2f7;
-  border: none;
-  color: white;
-  cursor: pointer;
+function showMessage() {
+  alert("Email: creaktube@gmail.com 🚀");
 }
