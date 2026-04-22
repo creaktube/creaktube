6a17a781241daca01a08982826a5ce837ccb1610
omarfaruk <creaktube@gmail.com>
2026-04-22 11:39:39 +0600

    updated name to Omar Faruk

---
+++ script.js
@@ -0,0 +1,15 @@
+body {
+  background-color: #1a1b26;
+  color: white;
+  text-align: center;
+  margin-top: 100px;
+  font-family: Arial;
+}
+
+button {
+  padding: 10px 20px;
+  background: #7aa2f7;
+  border: none;
+  color: white;
+  cursor: pointer;
+}
